3027a7ba8c91566a85b7c90c1ceff750982dcc3f
andrewneves7 <42624686+andrewneves7@users.noreply.github.com>
2023-02-13 10:11:17 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function displayText() {
+    document.getElementById("secret").innerHTML = "Will You Be My Valentine? <p>&hearts;<p>";
+}
+
+function removeText() {
+    document.getElementById("secret").innerHTML = "";
+}
